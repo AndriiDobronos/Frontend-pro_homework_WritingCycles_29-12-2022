06689fcfaeac4c843f5bdaf967d8e5eca41e1e6a
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-06 17:53:13 +0200

    corrected homework

--- WritingCycles.js
+++ WritingCycles.js
@@ -1,92 +1,103 @@
+//Написати цикли, які зможуть:
 
-let num = 10
+//Вивести на сторінку в один рядок через кому числа від 10 до 20.
+
+let numLowerLimiter = 10
 let i = 0
-let arr = []
-let arrSquare = []
-while (num <= 20) {
-    arr[i] = num
-    arrSquare[i] = num*num
+const arr = []
+const arrSquare = []
+while (numLowerLimiter <= 20) {
+    arr[i] = numLowerLimiter
+    arrSquare[i] = numLowerLimiter*numLowerLimiter
     i++
-    num++
+    numLowerLimiter++
 }
 alert(`Array numbers from 10 to 20: ${arr}`)
 /**************************************************************/
+//Вивести квадрати чисел від 10 до 20.
 alert(`Array square of numbers from 10 to 20: ${arrSquare}`)
 /*****************************************************************/
-let tablMultSeven = []
-let k = 1
-while (k <= 10) {
-    tablMultSeven[k-1] = `${k} * 7 = ${k * 7}\n`
-    k++
+//Вивести таблицю множення на 7.
+const tablMultSeven = []
+let multiplier = 1
+while (multiplier <= 10) {
+    tablMultSeven[multiplier - 1] = `${multiplier} * 7 = ${multiplier* 7}\n`
+    multiplier++
 }
 alert(`Multiplication table by 7:
  ${tablMultSeven.join(' ')}`)
 /******************************************************************/
-let n = 0
-let sum = 0
-while (n <= 15) {
-    sum = sum + n
-    n++
+//Знайти суму всіх цілих чисел від 1 до 15.
+let numElement = 0
+let sumElements = 0
+while (numElement <= 15) {
+    sumElements = sumElements + numElement
+    numElement++
 }
-alert(`Sum number from 1 to 15 : ${sum}`)
+alert(`Sum number from 1 to 15 : ${sumElements}`)
 /***************************************************************/
-let l = 15
+//Знайти добуток усіх цілих чисел від 15 до 35.
+let lowerNumber = 15
 let increase = 1
-while (l <= 35) {
-    increase = increase * l
-    l++
+while (lowerNumber <= 35) {
+    increase = increase * lowerNumber
+    lowerNumber++
 }
 alert(`Result increase number from 15 to 35 : ${increase}`)
 /*****************************************************************/
-let p = 1
+//Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
+let numItem = 1
 let arithmeticSum = 1
-while (p <= 500) {
-    arithmeticSum = arithmeticSum + p
-    p++
+while (numItem <= 500) {
+    arithmeticSum = arithmeticSum + numItem
+    numItem++
 }
 arithmeticMean = arithmeticSum / 500
 alert(`Arithmetic mean number from 1 to 500 : ${arithmeticMean} `)
 /*********************************************************************/
-let c = 30
+//Вивести суму лише парних чисел в діапазоні від 30 до 80.
+let initialElement = 30
 let sumEven = 0
-while (c <= 80) {
-    sumEven = sumEven + c
-    c = c + 2
+while (initialElement <= 80) {
+    sumEven = sumEven + initialElement
+    initialElement = initialElement + 2
 }
 alert(`Sum even number from 30 to 80 : ${sumEven}`)
 /*******************************************************************/
-let f = 100
-while (!Number.isInteger(f / 3 )) {
-    f = f + 1
+//Вивести всі числа в діапазоні від 100 до 200 кратні 3.
+let initialItem = 100
+while (!Number.isInteger(initialItem / 3 )) {
+    initialItem = initialItem + 1
 }
-let arrayMultOfThree = []
-let divisibleNumber = f
+const arrayMultOfThree = []
+let divisibleNumber = initialItem
 while (divisibleNumber <= 200) {
     arrayMultOfThree.push(divisibleNumber)
     divisibleNumber = divisibleNumber + 3
 }
-alert(`Divisible by three number from 100 to 200 : ${arrayMultOfThree}`)
+alert(`Divisible by three number from 
+100 to 200 : ${arrayMultOfThree}`)
 /*************************************************************************/
-let numberOfUser = prompt('Enter the number' +
-    'of divisors you want to know',"")
-
-if (numberOfUser === null || numberOfUser.trim() === "" ||
-    isNaN(+numberOfUser) === true) {
-    alert(`Entered number is not validation`)
-}
-let arrayDivisors = []
-let int = 0
-while (int <= Number(numberOfUser)) {
-    int++
-    if (Number.isInteger(+numberOfUser / int)) {
-        arrayDivisors.push(int)
+//Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
+const numberOfUser = prompt('Enter the number' +
+    'of divisors you want to know',"");
+if (!isNaN(+numberOfUser) === true && (+numberOfUser) > 0) {
+    const arrayDivisors = []
+    let possibleNumber = 0
+    while (possibleNumber <= Number(numberOfUser)) {
+        possibleNumber++
+        if (Number.isInteger(+numberOfUser / possibleNumber)) {
+            arrayDivisors.push(possibleNumber)
+        }
     }
-}
-alert(`This number : ${numberOfUser} can be divided into : ${arrayDivisors}`)
+    alert(`This number : ${numberOfUser} 
+    can be divided into : ${arrayDivisors}`)
+
 /**************************************************************/
+// Визначити кількість його парних дільників.
 let arrayDivisorsCopy = arrayDivisors.slice()
-let arrayDivisorsEven = []
-let arrayDivisorsOdd = []
+const arrayDivisorsEven = []
+const arrayDivisorsOdd = []
 for (let j = 0; j < arrayDivisorsCopy.length; j++) {
     if (Number.isInteger( arrayDivisorsCopy[j] / 2)) {
         arrayDivisorsEven.push(arrayDivisorsCopy[j])
@@ -94,34 +105,40 @@ for (let j = 0; j < arrayDivisorsCopy.length; j++) {
 }
 alert(`The number of even divisors is equal to : ${arrayDivisorsEven.length}`)
 /******************************************************************/
+//   Знайти суму його парних дільників.
 let sumArrayEven = 0
 for (let h = 0; h < arrayDivisorsEven.length; h++) {
     sumArrayEven = sumArrayEven + arrayDivisorsEven[h]
 }
 alert(`The sum even divisors is equal to: ${sumArrayEven}`)
+}
+else {
+    alert(`Entered number is not validation`)
+}
 /*******************************************************************/
-let multiplicationTable = []
-let  multiplicationTableAll = []
-let m = 1
-let d = 1
-while (m <= 11 && d <= 10) {
-multiplicationTable[(m - 1) + (d - 1)*10] = `${  m * d}`
-    if (m*d < 10) {
+//    Надрукувати повну таблицю множення від 1 до 10.
+const multiplicationTable = []
+const  multiplicationTableAll = []
+let row = 1
+let column = 1
+while (row <= 11 && column <= 10) {
+multiplicationTable[(row - 1) + (column - 1)*10] = `${  row * column}`
+    if (row*column < 10) {
         multiplicationTableAll.push('')
     }
-multiplicationTableAll.push(multiplicationTable[(m - 1) + (d - 1)*10])
-    m++
-    if (m === 11) {
+multiplicationTableAll.push(multiplicationTable[(row - 1) + (column - 1)*10])
+    row++
+    if (row === 11) {
         multiplicationTableAll.push(`\n`)
-        multiplicationTableAll.push(`${d + 1  }`)
-        d++
-        m = m - 10
-
+        multiplicationTableAll.push(`${column + 1  }`)
+        column ++
+        row = row - 10
     }
 }
+multiplicationTableAll.pop()
 alert(`Multiplication table :
         1    2    3    4    5    6    7    8    9  10
-  1  ${multiplicationTableAll.join('  ')}`)
+  1  ${(multiplicationTableAll.join('  '))}`)
 
 
 
